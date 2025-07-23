4b3a064c731f254a65a93465e293ee780d3b1f03
Saloni-coder03 <salonisinha474@gmail.com>
2025-07-23 21:54:23 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 
 // mongoDB connection url
-const mongoURL = process.env.MONGODB_URL_LOCAL;
-//const mongoURL = process.env.MONGODB_URL;
+//const mongoURL = process.env.MONGODB_URL_LOCAL;
+const mongoURL = process.env.MONGODB_URL;
 // for online database collection
 
 
